dd992f02fb9c1ad56423a0c4df6bdeb104606d68
Kendall <ktrudickm@gmail.com>
2023-01-05 12:22:50 -0800

    Cleaned up route code - refactored

--- app.js
+++ app.js
@@ -8,23 +8,12 @@ const app = express();
 // without middleware, when sending POST request, will get a res of undefined
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: "Hello from the server side!", app: "Natours" });
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("You can post to this endpoint...");
-// });
-
 // JSON.parse converts the json into a Javascript array of objects
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// route handler: (req, res)
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length, // tours is an array with multiple objects
@@ -32,28 +21,21 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTour = (req, res) => {
   // req.params is grabbing the :id
   console.log(req.params);
   // converting id from string to number
   const id = req.params.id * 1;
-  if (id > tours.length) {
+
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
     return res.status(404).json({
       status: "fail",
       message: "Invalid ID",
     });
   }
-
-  // OR (another method is if you put if statement after you assign tour after using find() below):
-  const tour = tours.find((el) => el.id === id);
-  //   if (!tour) {
-  //     return res.status(404).json({
-  //       status: "fail",
-  //       message: "Invalid ID",
-  //     });
-  //   }
 
   res.status(200).json({
     status: "success",
@@ -61,12 +43,10 @@ app.get("/api/v1/tours/:id", (req, res) => {
       tour,
     },
   });
-});
+};
 
-// req holds all data
-app.post("/api/v1/tours", (req, res) => {
+const createTour = (req, res) => {
   // console.log(req.body);
-
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -84,10 +64,9 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
 
-// patch just return the updated portion, not the entire object like put does
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
@@ -101,10 +80,9 @@ app.patch("/api/v1/tours/:id", (req, res) => {
       tour: "<Updated tour here...>",
     },
   });
-});
+};
 
-// delete request
-app.delete("/api/v1/tours/:id", (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
@@ -117,7 +95,22 @@ app.delete("/api/v1/tours/:id", (req, res) => {
     status: "success",
     data: null,
   });
-});
+};
+
+// app.get("/api/v1/tours", getAllTours);
+// app.post("/api/v1/tours", createTour);
+// app.get("/api/v1/tours/:id", getTour);
+// patch just return the updated portion, not the entire object like put does
+// app.patch("/api/v1/tours/:id", updateTour);
+// delete request
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 // Starts server
 const PORT = 3000;
