94ad1fcb11657ab81388edaaca322ab40645ea65
Kendall <ktrudickm@gmail.com>
2023-01-05 14:33:01 -0800

    Created and Mounted routers

--- app.js
+++ app.js
@@ -131,32 +131,32 @@ const getAllUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-    res.status(500).json({
-      status: "error",
-      message: "This route is not yet defined.",
-    });
-  };
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined.",
+  });
+};
 
-  const getUser = (req, res) => {
-    res.status(500).json({
-      status: "error",
-      message: "This route is not yet defined.",
-    });
-  };
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined.",
+  });
+};
 
-  const updateUser = (req, res) => {
-    res.status(500).json({
-      status: "error",
-      message: "This route is not yet defined.",
-    });
-  };
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined.",
+  });
+};
 
-  const deleteUser = (req, res) => {
-    res.status(500).json({
-      status: "error",
-      message: "This route is not yet defined.",
-    });
-  };
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined.",
+  });
+};
 
 // app.get("/api/v1/tours", getAllTours);
 // app.post("/api/v1/tours", createTour);
@@ -169,21 +169,19 @@ const createUser = (req, res) => {
 /////////////////////////////////////////////////////////////
 // 3) ROUTES
 
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-  .route("/api/v1/tours/:id")
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route("/").get(getAllTours).post(createTour);
 
-app.route("/api/v1/users").get(getAllUsers).post(createUser);
+tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
-app
-  .route("/api/v1/users/:id")
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route("/").get(getAllUsers).post(createUser);
+
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 //////////////////////////////////////////////////////////////
 // 4) Start server
