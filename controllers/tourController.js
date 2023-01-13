c1adb5126930c825f744b09db2d31990ec163146
Kendall <ktrudickm@gmail.com>
2023-01-12 18:55:43 -0800

    Added advanced sorting functionality for tours controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,35 +32,34 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-
-    console.log(req.query)
+    console.log(req.query);
 
     // BUILD QUERY
-    // 1) Filtering
+    // 1a) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advanced Filtering
+    // 1b) Advanced Filtering
 
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
     // { difficulty: 'easy', duration { $gte: 5 } }
     // { difficulty: 'easy', duration { gte: '5' } }
     // gte, gt, lte, lt
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
-  
+    // 2) Sorting
 
-    // OR:
-    // const tours = await Tour.find()
-    //   .where("duration")
-    //   .equals(5)
-    //   .where("difficulty")
-    //   .equals("easy");
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
