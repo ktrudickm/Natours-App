c5b04a6d359ce441266ea9c972f4e6ae1652ba8e
Kendall <ktrudickm@gmail.com>
2023-01-25 14:45:55 -0800

    Implemented Pagination querying

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,20 +55,33 @@ exports.getAllTours = async (req, res) => {
     // 2) Sorting
 
     if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
+      const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      query = query.sort("-createdAt _id");
     }
 
     // 3) Field Limiting
 
-    if (req.query.fields){
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields)
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
     } else {
       // exclude the __v field
-      query = query.select('-__v')
+      query = query.select("-__v");
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=3&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist.");
     }
 
     // EXECUTE QUERY
