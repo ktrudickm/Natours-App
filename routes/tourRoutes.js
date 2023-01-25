934f9f8969978e9016cae451729437578ed71a3e
Kendall <ktrudickm@gmail.com>
2023-01-25 15:20:32 -0800

    Consolidated API features code into class and placed in separate file in utils folder

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+
 // router.param('id', tourController.checkID);
 
 router.route("/").get(tourController.getAllTours).post(tourController.createTour);
